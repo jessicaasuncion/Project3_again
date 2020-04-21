62b468bcbda10bccac030f2f75859467c54c7652
Jamie J <jaydee12347@gmail.com>
2020-04-21 01:24:30 -0700

    Message page gui updated

---
+++ app/src/main/java/com/example/cs401_project3/MessageListActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs401_project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessageListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message_list_activity);
+    }
+}
